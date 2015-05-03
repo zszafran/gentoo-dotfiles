d9880ab92b49985a78f64c699c9ec44402e75b38
Zach Szafran <zszafran@gmail.com>
2015-05-03 15:49:58 -0400

    dotfiles

--- home/zms/tools/dotfile-submit.sh
+++ home/zms/tools/dotfile-submit.sh
@@ -1,1 +1,2 @@
-git --git-dir=/repo/.git --work-tree=/ commit -m "dotfiles" && git push upstream
+git --git-dir=/repo/.git --work-tree=/ commit -m "dotfiles" && \
+git --git-dir=/repo/.git --work-tree=/ push upstream
